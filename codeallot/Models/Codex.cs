55cbd9b9a5bc264bf2571dd7a14c80dded47a378
Ayush Rudani <ayushcrudani@gmail.com>
2023-03-03 22:37:34 +0530

    User & Codex Model Done

---
+++ codeallot/Models/Codex.cs
@@ -0,0 +1,15 @@
+﻿namespace codeallot.Models
+{
+    public class Codex
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public string? Filename { get; set; }
+        public string? Description { get; set; }
+        public string? Content { get; set; }
+        public string? CreatedAt { get; set; }
+        public string? UpdatedAt { get; set; }
+        public User? CreatedBy { get; set; }
+        public string? Link { get; set; }
+    }
+}
