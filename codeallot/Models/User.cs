2252503db9eb3e056fad4246bb76082fbc639a90
Ayush Rudani <ayushcrudani@gmail.com>
2023-03-03 22:04:25 +0530

    Project init

---
+++ codeallot/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace codeallot.Models
+{
+    public class User
+    {
+
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string? Name { get; set; }
+
+        public string? Status { get; set; }
+        public string? Linkedin { get; set; }
+        public string? Github { get; set; }
+        public string? CodexCount { get; set; }
+
+    }
+}
